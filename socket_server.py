3a70be8ce7443d4f425821fc48938b7199f2e1e5
22533788 <22533788@sun.ac.za>
2022-09-23 12:37:26 +0200

    server upgrades.

--- socket_server.py
+++ socket_server.py
@@ -1,8 +1,14 @@
 import socket
+import csv
+import datetime
+from os.path import exists
 
 HOST = "192.168.137.1"
 # HOST = "127.0.0.1"
 PORT = 65432 
+
+csv_file_name = 'sensor_data.csv'
+csv_header = ['time', 'uv', 'temperature', 'humidity']
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -10,11 +16,26 @@ s.listen(5)
 c, address = s.accept()
 print(f"Connection from {address} has been established.")
 
-while True:
-    # Keep reading while the client is writing.
-    data = c.recv(2048)
-    if not data:
-        # Client is done with sending.
-        break
-    print(data.decode("utf-8"))
+# ensure CSV is configured
+csv_exists = exists(f'./{csv_file_name}')
+if (not csv_exists):
+    with open(csv_file_name, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_header)
 
+# append to end of the CSV file
+with open(csv_file_name, 'a', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    while True:
+        # keep reading while client is writing
+        client_message_encoded = c.recv(2048)
+        if not client_message_encoded:
+            # when client is done writing
+            break
+        client_message = client_message_encoded.decode("utf-8")
+        csv_data = [str(datetime.datetime.now()), int(client_message), 0, 0]
+        print(csv_data)
+        writer.writerow(csv_data)
+
+
+print('Socket closed.')
